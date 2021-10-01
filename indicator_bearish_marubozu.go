d184402d74083a578f554044b9e1b1268e2adea6
Santosh Pillai <sapillai@redhat.com>
2021-10-01 10:37:55 +0530

    updated bullish and bearish marubozu with offset
    Signed-off-by: Santosh Pillai <sapillai@redhat.com>

--- indicator_bearish_marubozu.go
+++ indicator_bearish_marubozu.go
@@ -4,15 +4,21 @@ import "github.com/sdcoffey/big"
 
 type bearishMarubozuIndicator struct {
 	series *TimeSeries
+	offset big.Decimal
 }
 
 // NewBearishMarubozuIndicator returns and indicator with returns big.ONE if candle is bearish marubozu, else returns big.ZERO
-func NewBearishMarubozuIndicator(series *TimeSeries) Indicator {
-	return bearishMarubozuIndicator{series}
+func NewBearishMarubozuIndicator(series *TimeSeries, offset big.Decimal) Indicator {
+	return bearishMarubozuIndicator{series, offset}
 }
 
 func (b bearishMarubozuIndicator) Calculate(index int) big.Decimal {
 	if index == 0 {
+		return big.ZERO
+	}
+
+	// Return if candle is not bearish
+	if NewBearishIndicator(b.series).Calculate(index) != big.ONE {
 		return big.ZERO
 	}
 
@@ -21,8 +27,11 @@ func (b bearishMarubozuIndicator) Calculate(index int) big.Decimal {
 	currHighPrice := NewHighPriceIndicator(b.series).Calculate(index)
 	currLowPrice := NewLowPriceIndicator(b.series).Calculate(index)
 
+	isPureBearishMarubozu := currOpenPrice.EQ(currHighPrice) && currClosePrice.EQ(currLowPrice)
+	isBearishMarubuzo := (((currOpenPrice.Sub(currClosePrice)).Div(currHighPrice.Sub(currLowPrice))).Mul(big.NewFromInt(100))).GTE(b.offset)
+
 	// TODO: should we compare the candle height with average of pervious candles?
-	if currClosePrice.LT(currOpenPrice) && currOpenPrice.EQ(currHighPrice) && currClosePrice.EQ(currLowPrice) {
+	if isPureBearishMarubozu || isBearishMarubuzo {
 		return big.ONE
 	}
 
--- indicator_bullish_marubozu.go
+++ indicator_bullish_marubozu.go
@@ -1,18 +1,26 @@
 package techan
 
-import "github.com/sdcoffey/big"
+import (
+	"github.com/sdcoffey/big"
+)
 
 type bullishMarubozuIndicator struct {
 	series *TimeSeries
+	offset big.Decimal
 }
 
 // NewBullishMarubozuIndicator returns and indicator with returns big.ONE if candle is bullish marubozu, else returns big.ZERO
-func NewBullishMarubozuIndicator(series *TimeSeries) Indicator {
-	return bullishMarubozuIndicator{series}
+func NewBullishMarubozuIndicator(series *TimeSeries, offset big.Decimal) Indicator {
+	return bullishMarubozuIndicator{series, offset}
 }
 
 func (b bullishMarubozuIndicator) Calculate(index int) big.Decimal {
 	if index == 0 {
+		return big.ZERO
+	}
+
+	// Return if candle is not bullish
+	if NewBullishIndicator(b.series).Calculate(index) != big.ONE {
 		return big.ZERO
 	}
 
@@ -22,7 +30,10 @@ func (b bullishMarubozuIndicator) Calculate(index int) big.Decimal {
 	currLowPrice := NewLowPriceIndicator(b.series).Calculate(index)
 
 	// TODO: should we compare the candle height with average of pervious candles?
-	if currOpenPrice.LT(currClosePrice) && currOpenPrice.EQ(currLowPrice) && currClosePrice.EQ(currHighPrice) {
+
+	isPureBullishMarubozu := currOpenPrice.EQ(currLowPrice) && currClosePrice.EQ(currHighPrice)
+	isBullishMarubozu := ((currClosePrice.Sub(currOpenPrice).Div(currHighPrice.Sub(currLowPrice))).Mul(big.NewFromInt(100))).GTE(b.offset)
+	if isPureBullishMarubozu || isBullishMarubozu {
 		return big.ONE
 	}
 
